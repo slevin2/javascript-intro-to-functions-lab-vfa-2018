f29bed667fa1387791e1c17ec7d65f9a901ae3de
Sam Levin <sianlevin@gmail.com>
2018-06-29 22:02:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,14 @@ function logShout(string) {
 function logWhisper(string){
   return string.toLowerCase()
 }
+function sayHiToGrandma(string {
+  if (string.toUpperCase() === string) {
+    return "YES INDEED"
+}
 
+else if (string === "I love you, Grandma."){
+  return "I love you, too."
+}
+return "I can't hear you!"
+
+}
